794807e3808d547d3c22fac45d82684d8d588a0b
13782629534 <296918732@qq.com>
2020-02-18 17:01:05 +0800

    test3

--- src/main/java/com/woniu/godaji/GodajiApplication.java
+++ src/main/java/com/woniu/godaji/GodajiApplication.java
@@ -1,9 +1,11 @@
 package com.woniu.godaji;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.woniu.godaji.mapper")
 public class GodajiApplication {
 
     public static void main(String[] args) {
